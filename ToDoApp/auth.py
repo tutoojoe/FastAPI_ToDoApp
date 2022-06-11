fc606c3d5ab4cc986d3b3a4d31854c7fb8180eb5
Tutoo Joe Philip <tutoojoephilip@gmail.com>
2022-06-11 23:48:00 +0530

    user model and hashed pwd done

---
+++ ToDoApp/auth.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import models
+from passlib.context import CryptContext
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password):
+    return bcrypt_context.hash(password)
+
+
+app = FastAPI()
+
+
+@app.post("/create/user")
+async def create_new_user(create_user: CreateUser):
+    create_user_model = models.Users()
+    create_user_model.email = create_user.email
+    create_user_model.username = create_user.username
+    create_user_model.first_name = create_user.first_name
+    create_user_model.last_name = create_user.last_name
+    hash_password = get_password_hash(create_user.password)
+    create_user_model.hashed_password = hash_password
+    create_user_model.is_active = True
+
+    return create_user_model
--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,6 +1,19 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    todos = relationship("ToDos", back_populates="owner")
 
 class ToDos(Base):
     __tablename__ = "todos"
@@ -10,3 +23,7 @@ class ToDos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
+
